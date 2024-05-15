65e9f1246ed841ef5d8c19d62f0ccf7e101012ef
tkysnd <takuyasonoda222@gmail.com>
2024-05-15 21:38:20 +0900

    getMonthメソッドの利用方法修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,2 +1,2 @@
 const today = new Date();
-console.log(today.getFullYear()+'年'+today.getMonth()+'月'+today.getDate()+'日');
+console.log(today.getFullYear()+'年'+[today.getMonth()+1]+'月'+today.getDate()+'日');
