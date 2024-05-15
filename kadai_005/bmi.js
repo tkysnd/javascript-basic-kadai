81048338261cf06d7daf9dfed1afb2162abb04e5
tkysnd <takuyasonoda222@gmail.com>
2024-05-15 20:10:19 +0900

    課題提出用

---
+++ kadai_005/bmi.js
@@ -0,0 +1,6 @@
+let weight = 68; //kg
+let height = 1.7; //m
+
+let bmi;
+bmi=weight/(height**2);
+console.log(bmi);
---
+++ kadai_014/square.js
@@ -0,0 +1,7 @@
+const double =(numInput)=>{
+    let numOutput = numInput**2;
+    return numOutput;
+}
+
+const numInput = 10;
+console.log(double(numInput));
